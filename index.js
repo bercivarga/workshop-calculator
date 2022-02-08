e2346eadc2318942d64e4e576fa6f103df737d05
David van der Vlugt <david@spotr.ai>
2022-02-08 07:42:13 +0100

    Added js functions and event listeners

--- index.js
+++ index.js
@@ -1,5 +1,93 @@
 // Calculator project
-const display = document.querySelector('.display');
-const one = document.querySelector('.one');
 
-const total = 0;
+//Objects
+const display = document.querySelector('.display');
+const numberButtons = Array.from(document.getElementsByClassName('numberButton'));
+const operationButtons = Array.from(document.getElementsByClassName('operationButton'));
+const clearButton = document.querySelector('.clear')
+const calculateButton = document.querySelector('.equal')
+const calculator = {
+    previousNumber: '',
+    currentNumber: '',
+    total: '',
+    operation: ''
+}
+
+
+//Functions
+function updateDisplay (value) {
+    display.innerText = value
+} 
+
+function clearCalculator() {
+    calculator.previousNumber = ''
+    calculator.currentNumber = ''
+    calculator.total = ''
+    calculator.operation = ''
+    updateDisplay(0)
+}
+
+function calculateCalculator() {
+    console.log(calculator)
+    calculator.total = eval(
+        calculator.previousNumber 
+        + calculator.operation 
+        + calculator.currentNumber)
+    calculator.previousNumber = calculator.total
+    calculator.currentNumber = ''
+    updateDisplay (calculator.total)
+}
+
+function updateNumber(button) {
+    calculator.currentNumber = calculator.currentNumber + button.innerText
+    updateDisplay(calculator.currentNumber)
+}
+
+function updateOperation(button) {
+    calculator.operation = button.innerText
+    if (calculator.previousNumber === '') {
+        calculator.previousNumber = calculator.currentNumber
+        calculator.currentNumber = ''
+    }
+    else if (calculator.previousNumber != '' && calculator.currentNumber != '') {
+        calculateCalculator()
+        
+        updateDisplay(calculator.total)
+    }
+
+}
+
+
+// Event Listeners
+
+//Number buttons
+for (let i = 0; i < numberButtons.length; i++) {
+    numberButtons[i].addEventListener(
+        'click',
+        function() {
+            updateNumber(numberButtons[i])
+        }
+    )
+}
+
+//Clear button
+clearButton.addEventListener(
+    'click',
+    function() {clearCalculator()}
+)
+
+//Calculation button
+calculateButton.addEventListener(
+    'click',
+    function() {calculateCalculator()}
+)
+
+//Operation buttons 
+for (let i = 0; i < operationButtons.length; i++) {
+    operationButtons[i].addEventListener(
+        'click',
+        function() {
+            updateOperation(operationButtons[i])
+        }
+    )
+}
